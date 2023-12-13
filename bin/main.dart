ddb07bb4a02b6e166f61767135d208ac6d427b0f
joph <joogasl@gmail.com>
2023-12-13 17:22:47 -0500

    Updated app to latest version.

--- bin/main.dart
+++ bin/main.dart
@@ -10,18 +10,37 @@ late final TeleDart mera;
 Future<void> main(List<String> arguments) async {
   print("Initializing.");
   mera = await mera_bot.getBot();
+  final botInfo = await mera.getMe();
+  print("${botInfo.id} - ${botInfo.first_name} has started");
   final handlers = Handler(bot: mera);
+  print("Setiing up start handler.");
   mera.onCommand('start').listen(handlers.start);
+
+  print("Setiing up yt handler.");
   mera.onCommand('yt').listen(handlers.yt);
+
+  print("Setiing up inspire handler.");
   mera.onCommand('inspire').listen(handlers.inspire);
+
+  print("Setiing up book handler.");
   mera.onCommand('book').listen(handlers.book);
+
+  print("Setiing up 8b handler.");
   mera.onCommand('8b').listen(handlers.magic8b);
+
+  print("Setiing up img handler.");
   mera.onCommand('img').listen(handlers.img);
+
+  print("Setiing up define handler.");
   mera.onCommand('define').listen(handlers.define);
+
+  print("Setiing up spell handler.");
   mera.onCommand('spell').listen(handlers.spell);
+
+  print("Setiing up yt callback handler.");
   mera.onCallbackQuery().listen(handlers.ytCallback);
 
-  print("Starting bot ${(await mera.getMe()).username}");
+  print("Starting bot ${botInfo.username}");
   mera.start();
 
   ProcessSignal.sigint.watch().listen((event) {
